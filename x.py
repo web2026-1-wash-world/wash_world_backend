2adffc2b1dd52199cf0ba080173d5004a8b01de6
Oliver Valter <olva0001@stud.kea.dk>
2026-06-07 18:30:48 +0200

    Update sender email address in send_email function

--- x.py
+++ x.py
@@ -99,7 +99,7 @@ def send_email(subject, activation_email):
         # Copy the key : 
 
         # Email and password of the sender's Gmail account
-        sender_email = "rasmus.meinche@gmail.com"
+        sender_email = "olivervalter13@gmail.com"
         password = "dqid pudd nhdb wbmb"  # If 2FA is on, use an App Password instead
 
         # Receiver email address
